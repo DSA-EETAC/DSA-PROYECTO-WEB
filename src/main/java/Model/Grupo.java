5e3abd04e018de8b4ee9a2ee6168d54569d62551
aaronmarcos64 <aaron.marcos@estudiantat.upc.edu>
2026-06-18 16:33:00 +0200

    clase grupo

--- src/main/java/Model/Grupo.java
+++ src/main/java/Model/Grupo.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Grupo {
-    private String id;
+    private int id;
     private String nombre;
     private int miembros;
 
@@ -13,13 +13,13 @@ public class Grupo {
 
     // Le añadimos estas etiquetas al constructor con datos para que Jackson sepa mapearlo
     @JsonCreator
-    public Grupo(@JsonProperty("id") String id, @JsonProperty("nombre") String nombre) {
+    public Grupo(@JsonProperty("id") int id, @JsonProperty("nombre") String nombre) {
         this.id = id;
         this.nombre = nombre;
     }
 
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
     public int getMiembros() { return miembros; }
